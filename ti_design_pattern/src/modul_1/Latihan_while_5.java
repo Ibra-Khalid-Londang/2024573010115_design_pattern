a511f48f5954b334abc5ca95b6616e773f6fb92d
Ibra-Khalid-Londang <thekhaleedd@gmail.com>
2026-03-02 13:43:01 +0700

    quick review-pemrograman java dasar

---
+++ ti_design_pattern/src/modul_1/Latihan_while_5.java
@@ -0,0 +1,13 @@
+package modul_1;
+
+public class Latihan_while_5 {
+    public static void main (String[] args) {
+        int i = 1;
+        while (i <= 20) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
